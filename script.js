f6ab89ae94f830c901ca04b8c8155d2c26bd1136
Devin Mork <devin.mork@gmail.com>
2023-04-03 15:08:45 -0500

    Add buttons to drive game single round

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const moveOptions = ["rock", "paper", "scissors"]
+const moveOptions = ["rock", "paper", "scissors"];
+
+const buttonsContainer = document.querySelector('.player-selections');
+const results = document.createElement('p');
+buttonsContainer.appendChild(results);
+const selectionButtons = document.querySelectorAll(".selection-button");
+
+for (button of selectionButtons) {
+  button.addEventListener('click', function(e) {
+    let result = playOneRound(this.id, getComputerChoice());
+    results.textContent = result;
+  });
+}
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3)
@@ -21,39 +33,38 @@ function playOneRound(playerSelection, computerSelection) {
     finalMessage = "You won this round 😊";
     playerWon = 1;
   }
-  alert(finalMessage + `\n${playerSelection} vs. ${computerSelection}`);
-  return playerWon;
+  return finalMessage + `\n${playerSelection} vs. ${computerSelection}`;
 }
 
-function getPlayerSelection () {
-  while (true) {
-    let playerInput = prompt("Choose one:", "rock, paper, or scissors");
-    if (moveOptions.includes(playerInput)) {
-      return playerInput;
-    } else {
-      alert("That's not an option! Please choose rock, paper, or scissors.");
-    };
-  };
-}
+// function getPlayerSelection () {
+//   while (true) {
+//     let playerInput = prompt("Choose one:", "rock, paper, or scissors");
+//     if (moveOptions.includes(playerInput)) {
+//       return playerInput;
+//     } else {
+//       alert("That's not an option! Please choose rock, paper, or scissors.");
+//     };
+//   };
+// }
 
-function game() {
-  let playerWins = 0;
-  let computerWins = 0;
+// function game() {
+//   let playerWins = 0;
+//   let computerWins = 0;
 
-  while (playerWins < 5 && computerWins < 5) {
-    let gameResult = playOneRound(getPlayerSelection(), getComputerChoice());
-    if (gameResult == 1) {
-      playerWins ++;
-    } else if (gameResult == -1) {
-      computerWins ++;
-    }
-  }
+//   while (playerWins < 5 && computerWins < 5) {
+//     let gameResult = playOneRound(getPlayerSelection(), getComputerChoice());
+//     if (gameResult == 1) {
+//       playerWins ++;
+//     } else if (gameResult == -1) {
+//       computerWins ++;
+//     }
+//   }
 
-  let endingMessage = playerWins > computerWins ? 
-    "You won the game!" : "You lost the game.";
-  return endingMessage
-}
+//   let endingMessage = playerWins > computerWins ? 
+//     "You won the game!" : "You lost the game.";
+//   return endingMessage
+// }
 
-while (true) {
-  alert(game());
-}
+// while (true) {
+//   alert(game());
+// }
